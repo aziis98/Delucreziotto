26053e465fd3c657abab53350cdf6e8b4ff24c06
aziis98 <antonio.delucreziis@gmail.com>
2016-03-23 00:20:48 +0100

    Done more changes

--- server/public/scripts/home.js
+++ server/public/scripts/home.js
@@ -1,5 +1,17 @@
-angular.module('homeApp', [])
-  .controller('homeController', function () {
+angular.module('homeApp', []).controller('homeController', function () {
     var home = this;
+    var params = getParams();
     
-  })
+    home.matchKey = params['match'] || '';
+});
+  
+function getParams() {
+  var params = {};
+  var query = document.location.search.substring(1); // removes the pending '?'
+  var pairs = query.split('&');
+  pairs.forEach(function (pair) {
+    pair = pair.split('=');
+    params[pair[0]] = pair[1];
+  });
+  return params;
+}
